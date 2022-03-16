e75e4d23bc2c1b3adc6828e5504299052085ac2b
wyatt8740 <wyattfward@gmail.com>
2022-03-15 21:57:00 -0400

    burner update to match laptop's current version

--- icc-burn.py
+++ icc-burn.py
@@ -1,17 +1,28 @@
 #! /usr/bin/env python3
 import argparse
+from argparse import RawTextHelpFormatter
 from io import BytesIO # for BytesIO on embedded profiles
 from PIL import Image, ImageCms
-parser=argparse.ArgumentParser(description='Burns ICC profiles into images.')
+
+intent=ImageCms.INTENT_SATURATION
+
+parser=argparse.ArgumentParser(description='Burns ICC profiles into images.', formatter_class=RawTextHelpFormatter)
 parser.add_argument('--icc_in', dest='icc_in', type=str, nargs=1, default=None,
-                    help='ICC file to apply to the input. If none given, sRGB is automatically assumed unless there is an embedded profile.')
+                    help='ICC file to apply to the input. If none given, sRGB is\n\
+automatically assumed unless there is an embedded profile.')
+parser.add_argument('--intent', dest='intent', type=int, nargs=1, default=None,
+                    help='Which rendering intent to use for correction. Options are:\n\
+0 (Perceptual) (default)\n\
+1 (Relative Colorimetric)\n\
+2 (Saturation)\n\
+3 (Absolute Colorimetric)')
 parser.add_argument('icc_out', metavar='icc_out', type=str, nargs=1,
                     help='ICC file to apply to the output.')
 parser.add_argument('input_file', metavar='infile', type=str, nargs=1,
                     help='The file that needs an ICC profile burned into it.')
 parser.add_argument('output_file', metavar='outfile', type=str, nargs=1,
                     help='Where to place the picture with the burned-in profile.')
-parser.add_argument('--reverse', dest='reverse', action='store_true', default=False, help='Try to reverse the applied correction instead (inverse matrix).')
+parser.add_argument('--reverse', dest='reverse', action='store_true', default=False, help='Try to reverse the applied correction instead (inverse).')
 
 args=parser.parse_args()
 reverse=args.reverse
@@ -41,16 +52,22 @@ if icc_in is None:
     else:
         icc_in=ImageCms.createProfile("sRGB", -1)
 
+if args.intent is not None:
+    intent=args.intent[0]
+
 xform=None
 if reverse:
     print("Reverse mode.")
     xform=ImageCms.buildTransform(icc_out, icc_in,
                               input_file.mode, input_file.mode,
-                              ImageCms.INTENT_PERCEPTUAL, 0)
+                              intent, 0)
+#                              ImageCms.INTENT_PERCEPTUAL, 0)
 else:
     xform=ImageCms.buildTransform(icc_in, icc_out,
                               input_file.mode, input_file.mode,
-                              ImageCms.INTENT_PERCEPTUAL, 0)
+                              intent, 0)
+#                              ImageCms.INTENT_PERCEPTUAL, 0)
+
 
 img2=ImageCms.applyTransform(input_file, xform, inPlace=False)
 img2.save(output_file)
